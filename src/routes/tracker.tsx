dbf8c89e272c6641373a70e61e4fde91788644ee
Sergey Onufrienko <sergey@onufrienko.com>
2020-03-27 18:36:46 +0000

    Use LocationProvider

--- src/routes/tracker.tsx
+++ src/routes/tracker.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import Tracker from '../components/Tracker'
+import Tracker from '../components/Tracker';
+import { LocationProvider } from '../contexts/LocationContext';
 
 export default () => {
-  return <Box>
-    <Tracker />
-  </Box>;
+  return (
+    <Box>
+      <LocationProvider>
+        <Tracker />
+      </LocationProvider>
+    </Box>
+  );
 };
